89db0ea3bcb779f608b81f0b38de88bcaed668b9
Allan Chain <1599654796@qq.com>
2019-11-20 17:53:16 +0800

    add `ad_big_shift`

--- DAC.c
+++ DAC.c
@@ -58,36 +58,41 @@ void divide_big(struct BigInt x, int i,
     xlow->val = x.val;
     xhigh->val = x.val + x.len - xhigh->len;
 }
-struct BigInt add_big(struct BigInt a, struct BigInt b)
-{
+struct BigInt add_big_shift(struct BigInt a, struct BigInt b, int n){
     struct BigInt x, longer, shorter;
     short t, toadd=0;
+    x.len = a.len+n>b.len ? a.len+n : b.len;
+    x.val = (char*)malloc(x.len+1);
+    for (int i=0; i<n && b.len; i++){
+        *(x.val+i) = *b.val++;
+        b.len--;
+    }
     longer  = a.len > b.len ? a : b;
     shorter = a.len > b.len ? b : a;
-    // x.len may be added by 1 if needed
-    x.len = longer.len;
-    // In case it need one more digit
-    x.val = (char*)malloc(x.len+1);
     for (int i = 0; i<shorter.len; i++){
         t=*(a.val+i)+*(b.val+i)+toadd;
         if(t>99) toadd=1, t-=100;
         else toadd=0;
-        *(x.val+i)=t;
+        *(x.val+i+n)=t;
     }
     // incase it need `jin wei`
     // and optimise a little
     for (int i = shorter.len; i<longer.len; i++){
         if (toadd){
-            if(*(a.val+i)==99) *(x.val+i)=0;
-            else *(x.val+i)=*(a.val+i)+toadd--;
+            if(*(longer.val+i)==99) *(x.val+i+n)=0;
+            else *(x.val+i+n)=*(longer.val+i)+toadd--;
         }
-        else *(x.val+i)=*(a.val+i);
+        else *(x.val+i+n)=*(longer.val+i);
     }
     if (toadd)
         // longer x here
         *(x.val+x.len++)=toadd;
     // free a and b?
     return x;
+}
+struct BigInt add_big(struct BigInt a, struct BigInt b)
+{
+    return add_big_shift(a, b, 0);
 }
 struct BigInt mul_big(struct BigInt a, struct BigInt b)
 {
@@ -96,25 +101,25 @@ struct BigInt mul_big(struct BigInt a, struct BigInt b)
     if (maxlen==1) return int2big(*a.val**b.val);
     struct BigInt alow, ahigh, blow, bhigh;
     n=maxlen/2;
-    devide_big(a, n, &alow, &ahigh);
-    devide_big(b, n, &blow, &bhigh);
+    divide_big(a, n, &alow, &ahigh);
+    divide_big(b, n, &blow, &bhigh);
     struct BigInt P1, P2, P3;
     P1 = mul_big(ahigh, bhigh);
     P2 = mul_big(alow, blow);
-    P3 = mul_big(add_big(alow, ahigh), add_big(b_low, b_high));
+    P3 = mul_big(add_big(alow, ahigh), add_big(blow, bhigh));
     return P1;
 }
 
 int main(void)
 {
     char a0[] = "1156869";
-    char b0[] = "56869";
+    char b0[] = "123456869";
     struct BigInt a, b, c, d;
     a = parse_big(a0);
     b = parse_big(b0);
     print_big(a);
     print_big(b);
-    print_big(add_big(a, b));
+    print_big(add_big_shift(a, b, 1));
     divide_big(b, 2, &c, &d);
     print_big(c);
     print_big(d);
