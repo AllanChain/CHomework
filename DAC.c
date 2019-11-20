9d0028b998878da5fa6d5f7d2161e21e57499fb2
Allan Chain <1599654796@qq.com>
2019-11-20 19:19:06 +0800

    complete mul

--- DAC.c
+++ DAC.c
@@ -33,34 +33,51 @@ struct BigInt parse_big(char *a)
 struct BigInt int2big(int a)
 {
     struct BigInt x;
-    x.len=1;
-    x.val=(char*)malloc(1);
-    *x.val=a;
+    if (a<100){
+        x.len=1;
+        x.val=(char*)malloc(1);
+        *x.val=a;
+    } else {
+        x.len=2;
+        x.val=(char*)malloc(2);
+        *x.val=a%100;
+        *(x.val+1)=a/100;
+    }
     return x;
 }
 void print_big(struct BigInt x)
 {
-    for (int i = x.len - 1; i >= 0; i--)
-        printf("%d", *(x.val + i));
+    if (x.len==0){
+        printf("0");
+        return;
+    }
+    // 0 is needed only in the later
+    printf("%d", *(x.val + x.len -1));
+    for (int i = x.len - 2; i >= 0; i--)
+        printf("%02d", *(x.val + i));
     printf("\n");
 }
-
+/* dividing x to xlow (lowest `0` to higher i digit exclusive)[len=i]
+   and xhigh (i th inclusive to the highest `len`)[len=x.len-i]
+   But... is it okay if one of them is empty? or i larger than x.len? */
 void divide_big(struct BigInt x, int i,
                 struct BigInt *xlow, struct BigInt *xhigh)
 {
     if (i>=x.len){
-        xlow->len = 0;
-        xhigh->len = x.len;
+        xlow->len = x.len;
+        xhigh->len = 0;
     }else{
-        xlow->len = x.len - i;
-        xhigh->len = i;
+        xlow->len = i;
+        xhigh->len = x.len - i;
     }
     xlow->val = x.val;
-    xhigh->val = x.val + x.len - xhigh->len;
+    xhigh->val = x.val + i;
 }
 struct BigInt add_big_shift(struct BigInt a, struct BigInt b, int n){
     struct BigInt x, longer, shorter;
     short t, toadd=0;
+    if (a.len==0) return b;
+    if (b.len==0) return a;
     x.len = a.len+n>b.len ? a.len+n : b.len;
     x.val = (char*)malloc(x.len+1);
     for (int i=0; i<n && b.len; i++){
@@ -94,10 +111,38 @@ struct BigInt add_big(struct BigInt a, struct BigInt b)
 {
     return add_big_shift(a, b, 0);
 }
+char digit(struct BigInt a, int i)
+{
+    if (i<a.len) return *(a.val+i);
+    else return 0;
+}
+struct BigInt sub_big(struct BigInt a, struct BigInt b)
+{
+    struct BigInt x, longer, shorter;
+    short t, tosub=0;
+    x.len = a.len;
+    x.val = (char*)malloc(x.len);
+    for (int i = 0; i<a.len; i++){
+        if(*(a.val+i) >= digit(b, i)+tosub){
+            *(x.val+i)=*(a.val+i)-digit(b, i)-tosub;
+            tosub=0;
+        } else{
+            *(x.val+i)=100+*(a.val+i)-digit(b, i)-tosub;
+            tosub=1;
+        }
+    }
+    for (int i=x.len-1; i>=0 && *(x.val+i)==0; i--) x.len--;
+    return x;
+}
 struct BigInt mul_big(struct BigInt a, struct BigInt b)
 {
     int maxlen, n;
     maxlen = a.len > b.len ? a.len : b.len;
+    if (a.len==0 || b.len==0){
+        struct BigInt x;
+        x.len=0;
+        return x;
+    }
     if (maxlen==1) return int2big(*a.val**b.val);
     struct BigInt alow, ahigh, blow, bhigh;
     n=maxlen/2;
@@ -107,21 +152,16 @@ struct BigInt mul_big(struct BigInt a, struct BigInt b)
     P1 = mul_big(ahigh, bhigh);
     P2 = mul_big(alow, blow);
     P3 = mul_big(add_big(alow, ahigh), add_big(blow, bhigh));
-    return P1;
+    return add_big_shift(add_big_shift(P1, sub_big(sub_big(P3, P1), P2), n), P2, n);
 }
 
 int main(void)
 {
-    char a0[] = "1156869";
-    char b0[] = "123456869";
+    char a0[] = "1145672";
+    char b0[] = "86457";
     struct BigInt a, b, c, d;
     a = parse_big(a0);
     b = parse_big(b0);
-    print_big(a);
-    print_big(b);
-    print_big(add_big_shift(a, b, 1));
-    divide_big(b, 2, &c, &d);
-    print_big(c);
-    print_big(d);
+    print_big(mul_big(a, b));
     return 0;
 }
