cc74c3371a2b79679c501535e5bfeca75583e218
Allan Chain <1599654796@qq.com>
2019-11-20 10:03:13 +0800

    format DAC source file

--- DAC.c
+++ DAC.c
@@ -1,47 +1,53 @@
 #include <stdio.h>
 #include <malloc.h>
 
-struct BigInt{
+struct BigInt
+{
     int len;
     char *val;
 };
 
 void parse_big(char *a, struct BigInt *x)
 {
-    char *p=a;
+    char *p = a;
     int len;
-    while(*p++);
-    p-=2;  //now p is the last char
-    len = p-a+1;
+    while (*p++)
+        ;
+    p -= 2; //now p is the last char
+    len = p - a + 1;
     // add one for another digit if odd
-    len = len%2==0?len/2:(len+1)/2;
-    static char *xp=NULL;
-    xp=(char*)malloc(100);
-    x->val=xp; x->len=len;
-    while(p>a){
-        *xp++=(*p-'0')+(*(p-1)-'0')*10;
-        p-=2;
+    len = len % 2 == 0 ? len / 2 : (len + 1) / 2;
+    char *xp = (char *)malloc(len);
+    x->val = xp;
+    x->len = len;
+    while (p > a)
+    {
+        *xp++ = (*p - '0') + (*(p - 1) - '0') * 10;
+        p -= 2;
     }
-    if(p==a) *xp++=*p-'0';
+    if (p == a)
+        *xp++ = *p - '0';
 }
 void print_big(struct BigInt x)
 {
-    for(int i=x.len-1; i>=0; i--)
-        printf("%d", *(x.val+i));
+    for (int i = x.len - 1; i >= 0; i--)
+        printf("%d", *(x.val + i));
     printf("\n");
 }
 
 /* dividing x to xlow (lowest `0` to higher i digit exclusive)[len=i]
    and xhigh (i th inclusive to the highest `len`)[len=x.len-i] */
 void divide_big(struct BigInt x, int i,
-        struct BigInt *xlow, struct BigInt *xhigh)
+                struct BigInt *xlow, struct BigInt *xhigh)
 {
-    xlow->len=i; xhigh->len=x.len-i;
-    xlow->val=x.val; xhigh->val=x.val+i;
+    xlow->len = i;
+    xhigh->len = x.len - i;
+    xlow->val = x.val;
+    xhigh->val = x.val + i;
 }
 int main(void)
 {
-    char a[]="11256869";
+    char a[] = "11256869";
     struct BigInt b, c, d;
     parse_big(a, &b);
     print_big(b);
